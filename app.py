7f658ed7ebae19f65025dede6b8fe1b429337331
Csaba HITTER <csaba@hitter.ro>
2025-01-30 15:44:33 +0200

    design mod

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ import hashlib
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'another_strong_key_here_supersecretkey')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(
-    'DATABASE_URL', 'mysql+pymysql://rss_db_user:rssdbuserpassword@mariadb/rss_db'
+    'DATABASE_URL', 'mysql+pymysql://rss_db_user:rssdbuserpassword@localhost/rss_db'
 )
 
 # Initialize extensions
